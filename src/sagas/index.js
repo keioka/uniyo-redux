6d86f1947996eb51e1c6ce4e085ae0d03fa7a30f
K <koka0828us@gmail.com>
2017-05-11 17:31:24 -0700

    Add new sagas

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,12 @@
 import { schoolsSearchSaga, schoolInfoSaga } from './schools'
 import { logInSaga, userCreateSaga, tokenRefreshSaga, currentUserSaga } from './auth'
-import { addUserTagsSaga, updateUserProfilePicSaga } from './profile'
-import { postsSearchSaga, postCreateSaga } from './post'
+import { addUserTagsSaga, hashtagDeleteSaga, hashtagsUserSaga, updateUserProfilePicSaga } from './profile'
+import { postInfoSaga, postsSearchSaga, postCreateSaga, answerSearchSaga, answerCreateSaga } from './post'
 import { commentsSearchSaga, commentCreateSaga } from './comment'
 import { userInfoSaga, userInfoAsync, userSearchSaga } from './user'
+import { messageSearchSaga, messageCreateSaga } from './message'
+import { channelSearchSaga, channelCreateSaga } from './channel'
+import { hashtagTrendingSearchSaga } from './hashtag'
 
 export default {
   // school search
@@ -16,11 +19,19 @@ export default {
   currentUserSaga,
   tokenRefreshSaga,
   addUserTagsSaga,
+  hashtagDeleteSaga,
+  hashtagsUserSaga,
   updateUserProfilePicSaga,
 
+  // hashtag
+  hashtagTrendingSearchSaga,
+
   // post
+  postInfoSaga,
   postsSearchSaga,
   postCreateSaga,
+  answerSearchSaga,
+  answerCreateSaga,
 
   // comment
   commentsSearchSaga,
@@ -30,4 +41,12 @@ export default {
   userInfoAsync,
   userInfoSaga,
   userSearchSaga,
+
+  // message
+  messageSearchSaga,
+  messageCreateSaga,
+
+  // channel
+  channelSearchSaga,
+  channelCreateSaga,
 }

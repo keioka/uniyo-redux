c13d18725a414b08eb6d13b89e8656e9cc550f7a
K <koka0828us@gmail.com>
2017-05-11 17:23:27 -0700

    Modify post redux

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,15 +1,36 @@
 import { actionTypes } from './'
 
+
 /**
- * Requests posts Search
+ * Requests posts info
  *
  * @param {object} params
  * @returns {object} action
  */
+
+export const postInfo = params => ({
+  type: actionTypes.postInfo.request,
+  ...params,
+})
+
+/**
+ * Requests posts search
+ *
+ * @param {object} params
+ * @returns {object} action
+ */
+
 export const postsSearch = params => ({
   type: actionTypes.postsSearch.request,
   ...params,
 })
+
+/**
+ * Requests posts create
+ *
+ * @param {object} params
+ * @returns {object} action
+ */
 
 export const postCreate = params => ({
   type: actionTypes.postCreate.request,
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -17,6 +17,27 @@ const initialState = Immutable({
  */
 const posts = (state = initialState, action) => {
   switch (action.type) {
+
+    case actionTypes.postInfo.request: {
+      return Immutable(state).merge({
+        fetching: true,
+      })
+    }
+
+    case actionTypes.postInfo.success: {
+      return Immutable(state).merge({
+        all: _.uniqBy([action.result.data, ...state.all], data => data.id),
+        fetching: false,
+      })
+    }
+
+    case actionTypes.postInfo.error: {
+      return Immutable(state).merge({
+        error: action.error,
+        fetching: false,
+      })
+    }
+
     case actionTypes.postsSearch.request: {
       return Immutable(state).merge({
         fetching: true,

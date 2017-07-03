b92d4e738d9e8ef34c91869eeff24d5e7b167156
K <koka0828us@gmail.com>
2017-07-03 11:39:45 -0700

    Add staging setting

--- src/config.js
+++ src/config.js
@@ -1,3 +1,16 @@
+var debug = require('debug')('redux:cofig/path')
+
+let path
+if (__STG__) {
+  path = 'https://staging-api.uniyo.io/v1'
+} else if (__PROD__) {
+  path = 'https://api.uniyo.io/v1'
+} else {
+  path = 'https://api.uniyo.io/v1'
+}
+
+debug(path)
+
 export default {
-  path: 'https://api.uniyo.io/v1',
+  path,
 }

b950d8802be77d344351cac4b85709d9ff6d969b
K <koka0828us@gmail.com>
2017-07-07 15:15:50 -0700

    Delete answer reducers and moved them to post reducer

--- src/reducers/answers.js
+++ src/reducers/answers.js
@@ -16,47 +16,6 @@ const initialState = Immutable({
  */
 const answers = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.answerSearch.request: {
-      return Immutable(state).merge({
-        fetching: true
-      })
-    }
-
-    case actionTypes.answerSearch.success: {
-      return Immutable(state).merge({
-        all: _.uniqBy([...state.all, ...action.result.data], data => data.id),
-        fetching: false,
-      })
-    }
-
-    case actionTypes.answerSearch.error: {
-      return Immutable(state).merge({
-        error: action.error,
-        fetching: false,
-      })
-    }
-
-    case actionTypes.answerCreate.request: {
-      return Immutable(state).merge({
-        fetching: true
-      })
-    }
-
-    case actionTypes.answerCreate.success: {
-      return Immutable(state).merge({
-        all: _.uniqBy([...state.all, action.result.data], data => data.id),
-        fetching: false,
-      })
-    }
-
-    case actionTypes.answerCreate.error: {
-      return Immutable(state).merge({
-        error: action.error,
-        fetching: false,
-      })
-    }
-
-
     default: {
       return state
     }
